10a9e038135105bd6a99dd5ad762c140a9bb6fdf
Rafan <rafanmu33@gmail.com>
2025-10-07 12:46:39 +0530

    updaed cors config in backend

--- backend/src/configs/cors.config.ts
+++ backend/src/configs/cors.config.ts
@@ -1,9 +1,8 @@
 import { env } from "./env.config";
 
 export const corsConfig = {
-  // origin: "http://localhost:3000",
   origin: env.CLIENT_ORIGIN,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  allowedHeaders: ["Content-Type"],
+  allowedHeaders: ["Content-Type","Authorization"],
 };
