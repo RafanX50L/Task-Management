90a9ed76b1fecc615e3fee21e915acd0a134407c
Rafan <rafanmu33@gmail.com>
2025-10-07 12:16:17 +0530

    create an admin creation dummy

--- backend/src/services/implementation/auth.service.ts
+++ backend/src/services/implementation/auth.service.ts
@@ -6,6 +6,7 @@ import {
   createHttpError,
   generateAccessToken,
   generateRefreshToken,
+  hashPassword,
   verifyRefreshToken,
 } from "@/utils";
 import { HttpResponse, HttpStatus } from "@/constants";
@@ -20,7 +21,7 @@ export class AuthService implements IAuthService {
     password: string
   ): Promise<{ accessToken: string; refreshToken: string; user: IUser }> {
     const user = await this._userRepository.findByEmail(identifier);
-    await this._userRepository.create({email: "admin@gmail.com", password: "Admin@123", role: "admin"});
+    await this._userRepository.create({email: "admin@gmail.com", password: await hashPassword("Admin@123"), role: "admin"});
 
     if (!user) {
       throw createHttpError(HttpStatus.NOT_FOUND, HttpResponse.USER_NOT_FOUND);
