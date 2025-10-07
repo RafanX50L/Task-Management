28fe7632b29f8aad3ec42243be8d9ef27bde3582
Rafan <rafanmu33@gmail.com>
2025-10-07 13:25:52 +0530

    updated cookie setting

--- backend/src/utils/refresh-cookie.util.ts
+++ backend/src/utils/refresh-cookie.util.ts
@@ -4,13 +4,12 @@ import { Response } from "express";
 export function setCookie(res: Response, refreshToken: string, entity: string) {
   res.cookie(entity, refreshToken, {
     httpOnly: true,
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-    secure: false,
-    sameSite: "strict",
-    // secure: process.env.NODE_ENV === "production",
-    // sameSite: env.NODE_ENV === "production" ? "none" : "strict",
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+    secure: true,                     // must be true on HTTPS
+    sameSite: "none",                 // allow cross-origin cookies
   });
 }
+
 
 export function deleteCookie(res: Response, entity: string) {
   res.clearCookie(entity, {
