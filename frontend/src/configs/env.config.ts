e7cb9bb2b1110391c910566cdc21e53d396b40c3
Rafan <rafanmu33@gmail.com>
2025-10-07 10:48:39 +0530

    updated env config in frontend

--- frontend/src/configs/env.config.ts
+++ frontend/src/configs/env.config.ts
@@ -1,9 +1,5 @@
 export const env = {
   get SERVER_ORIGIN() {
-    // return import.meta.env.VITE_SERVER_ORIGIN;
-    return "http://localhost:4000";
-    // return "http://3.111.41.75";
+    return import.meta.env.VITE_SERVER_ORIGIN;
   },
 };
-
-console.log(import.meta.env.VITE_SERVER_ORIGIN);
