dc2dc11485a63bee5d9c5433e6427b2bdcbed6d8
Rafan <rafanmu33@gmail.com>
2025-10-07 12:23:35 +0530

    removed admin creation dummy

--- backend/src/services/implementation/auth.service.ts
+++ backend/src/services/implementation/auth.service.ts
@@ -6,7 +6,6 @@ import {
   createHttpError,
   generateAccessToken,
   generateRefreshToken,
-  hashPassword,
   verifyRefreshToken,
 } from "@/utils";
 import { HttpResponse, HttpStatus } from "@/constants";
@@ -21,7 +20,6 @@ export class AuthService implements IAuthService {
     password: string
   ): Promise<{ accessToken: string; refreshToken: string; user: IUser }> {
     const user = await this._userRepository.findByEmail(identifier);
-    await this._userRepository.create({email: "admin@gmail.com", password: await hashPassword("Admin@123"), role: "admin", username: "admin"});
 
     if (!user) {
       throw createHttpError(HttpStatus.NOT_FOUND, HttpResponse.USER_NOT_FOUND);
